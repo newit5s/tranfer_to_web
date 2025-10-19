1d8dd9ca4b5cb064b3e652da1b7310b14c8a5953
newit5s <85056668+newit5s@users.noreply.github.com>
2025-10-19 13:55:04 +0700

    Fix language switcher widget loading

--- includes/class-language-switcher.php
+++ includes/class-language-switcher.php
@@ -232,67 +232,14 @@ class RB_Language_Switcher {
      * Register widget
      */
     public function register_widget() {
-        register_widget('RB_Language_Switcher_Widget');
-    }
-}
+        if (!class_exists('WP_Widget')) {
+            return;
+        }
 
-/**
- * Language Switcher Widget
- */
-class RB_Language_Switcher_Widget extends WP_Widget {
-    
-    public function __construct() {
-        parent::__construct(
-            'rb_language_switcher_widget',
-            rb_t('language_switcher'),
-            array('description' => rb_t('language_switcher_description'))
-        );
-    }
-    
-    public function widget($args, $instance) {
-        echo $args['before_widget'];
-        
-        if (!empty($instance['title'])) {
-            echo $args['before_title'] . apply_filters('widget_title', $instance['title']) . $args['after_title'];
+        if (!class_exists('RB_Language_Switcher_Widget')) {
+            require_once plugin_dir_path(__FILE__) . 'widgets/class-language-switcher-widget.php';
         }
-        
-        $switcher = new RB_Language_Switcher();
-        $style = !empty($instance['style']) ? $instance['style'] : 'dropdown';
-        
-        if ($style === 'flags') {
-            $switcher->render_flags();
-        } else {
-            $switcher->render_dropdown();
-        }
-        
-        echo $args['after_widget'];
-    }
-    
-    public function form($instance) {
-        $title = !empty($instance['title']) ? $instance['title'] : rb_t('language');
-        $style = !empty($instance['style']) ? $instance['style'] : 'dropdown';
-        ?>
-        <p>
-            <label for="<?php echo $this->get_field_id('title'); ?>"><?php rb_e('title'); ?>:</label>
-            <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" 
-                   name="<?php echo $this->get_field_name('title'); ?>" type="text" 
-                   value="<?php echo esc_attr($title); ?>">
-        </p>
-        <p>
-            <label for="<?php echo $this->get_field_id('style'); ?>"><?php rb_e('style'); ?>:</label>
-            <select class="widefat" id="<?php echo $this->get_field_id('style'); ?>" 
-                    name="<?php echo $this->get_field_name('style'); ?>">
-                <option value="dropdown" <?php selected($style, 'dropdown'); ?>><?php rb_e('dropdown'); ?></option>
-                <option value="flags" <?php selected($style, 'flags'); ?>><?php rb_e('flags'); ?></option>
-            </select>
-        </p>
-        <?php
-    }
-    
-    public function update($new_instance, $old_instance) {
-        $instance = array();
-        $instance['title'] = (!empty($new_instance['title'])) ? strip_tags($new_instance['title']) : '';
-        $instance['style'] = (!empty($new_instance['style'])) ? strip_tags($new_instance['style']) : 'dropdown';
-        return $instance;
+
+        register_widget('RB_Language_Switcher_Widget');
     }
 }
